3eb22fde10c39397d4a6ba3e11369cf1c4191782
Shailendrajain08 <71479782+Shailendrajain08@users.noreply.github.com>
2023-12-19 23:10:03 +0000

    Variables Class and understanding

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553;
+let email = "shailendra456@gmail.com";
+var accountPassword = "123456";
+accountCity = "Damoh";
+
+
+// accountId = 1445522; Assignment other value to const is not allowed
+
+email = "shailendra456@gmail.com";
+accountPassword = "12345";
+accountCity = "Bhopal"
+console.table([accountId, email, accountPassword, accountCity])
+
+/*
+prefer not to use var 
+beacuse of issue in block scope and functional scope
+*/ 
