44f73870303bcaf861a76321bc1400640d997cac
Shailendrajain08 <71479782+Shailendrajain08@users.noreply.github.com>
2023-12-20 22:15:02 +0000

    Datatypes understanding

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // it treats all JS code as newer version;
+
+//alert (3+3) ; // we are using nodejs and browser node js not allow us to use alert only.
+
+// console.log(3
+    // +3 ) // code readability should be high. 
+
+let name = "Shailendra jain"  // String with ""
+let age = 28;                 // number without ""
+let isLogged = false;             // boolean only true or false;
+
+// number => upto 2 power 53
+// bigint => if value is more then 2 power 53
+// string => with ""
+// boolean => true/false
+// null => standalone value
+// undefined => Not defined value;
+// symbol => unique
+
+// Object
+
+console.log(typeof undefined)
+console.log(typeof null)
