3ac4055da09309fa34505434d2435a4d359d6155
Shailendrajain08 <71479782+Shailendrajain08@users.noreply.github.com>
2023-12-20 23:06:02 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_datatypes_conversion.js
@@ -0,0 +1,35 @@
+let score = "62abc";
+
+// console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// If we convert values in datatypes we got output as
+//"62" => 33
+//"62abc" => NaN
+//true => 1, false => 0;
+//Null => 0
+// undefined => NaN
+
+let isLoggedIn = "1";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "1" => true
+// "shailendra" => true
+
+let someNumber = 33
+let stringSomeNumber = String(someNumber)
+
+
+console.log(stringSomeNumber)
+console.log(typeof stringSomeNumber)
