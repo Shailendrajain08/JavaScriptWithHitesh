89bba98edbaf48aa715a60baa313e4aa4755e0e8
Shailendrajain08 <71479782+Shailendrajain08@users.noreply.github.com>
2023-12-21 21:46:07 +0000

    Datatype Operations

--- 01_basics/03_datatypes_conversion.js
+++ 01_basics/03_datatypes_conversion.js
@@ -31,5 +31,47 @@ let someNumber = 33
 let stringSomeNumber = String(someNumber)
 
 
-console.log(stringSomeNumber)
-console.log(typeof stringSomeNumber)
+// console.log(stringSomeNumber)
+// console.log(typeof stringSomeNumber)
+
+
+// ********************* Operations *********************
+
+let value = 3;
+let negvalue = -value;
+
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+str1 = "Shailendra"
+str2 = "Hello"
+
+str3 = str2 + str1
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log("1" + "2")
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(3 + 4 * 5 % 3)
+
+// console.log(+true)
+// console.log(+"")
+
+// gameCounter = 100
+
+// gameCounter++
+
+// console.log(gameCounter);
+
+// ++gameCounter
+
+// console.log(gameCounter);
